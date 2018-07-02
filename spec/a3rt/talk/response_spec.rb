7ffe31339f64e4527eb5549c44aa070400d78e1e
Tomoya Chiba <tomo.asleep@gmail.com>
2018-07-02 23:13:13 +0900

    Refactor response_spec

--- spec/a3rt/talk/response_spec.rb
+++ spec/a3rt/talk/response_spec.rb
@@ -1,7 +1,5 @@
 describe A3rt::Talk::Response do
-  subject { A3rt::Talk::Response.new(raw_response) }
-
-  let(:raw_response) do
+  let(:raw_success_response) do
     {
       'status' => 0,
       'message' => 'ok',
@@ -12,8 +10,12 @@ describe A3rt::Talk::Response do
     }
   end
 
-  it do
-    least = subject.least_perplex
-    expect(least.reply).to eq 'test2'
+  describe '#least_perplex' do
+    let(:response) { A3rt::Talk::Response.new(raw_success_response) }
+    subject { response.least_perplex }
+
+    it 'returns the result with least perplex' do
+      expect(subject).to have_attributes(reply: 'test2', perplexity: 0.2)
+    end
   end
 end
