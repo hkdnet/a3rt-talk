f604ad2bb606f43f4dd140293581acd454a54376
Tomoya Chiba <tomo.asleep@gmail.com>
2018-07-02 23:13:28 +0900

    Add success? method to A3rt::Talk::Response

--- spec/a3rt/talk/response_spec.rb
+++ spec/a3rt/talk/response_spec.rb
@@ -10,12 +10,33 @@ describe A3rt::Talk::Response do
     }
   end
 
+  let(:raw_failure_response) do
+    {
+      'status' => 1001,
+      'message' => 'apikey not found',
+    }
+  end
+
   describe '#least_perplex' do
     let(:response) { A3rt::Talk::Response.new(raw_success_response) }
     subject { response.least_perplex }
 
     it 'returns the result with least perplex' do
       expect(subject).to have_attributes(reply: 'test2', perplexity: 0.2)
+    end
+  end
+
+  describe '#success?' do
+    subject { response.success? }
+
+    context 'when the response status is 0' do
+      let(:response) { A3rt::Talk::Response.new(raw_success_response) }
+      it { is_expected.to be_truthy }
+    end
+
+    context 'when the response status is not 0' do
+      let(:response) { A3rt::Talk::Response.new(raw_failure_response) }
+      it { is_expected.to be_falsy }
     end
   end
 end
